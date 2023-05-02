578d5dc6b8f0ffd3ef37e16725f7905390a196dc
dev-edsonlopes <dev_edson@outlook.com>
2023-05-02 10:23:23 -0300

    Review React

---
+++ aula_nextjs/fundamentos/src/pages/estilo.jsx
@@ -0,0 +1,8 @@
+import estilos from './Estilo.module.css'
+export default function Estilo() {
+    return (
+        <div className= { estilos.roxo }>
+            <h1>Estilo usando CSS Modulos</h1>
+        </div>
+    )
+}
---
+++ aula_nextjs/fundamentos/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Inicio() {
+    return (
+        <div>
+            <h1>Fundamentos de Next.JS & React</h1>
+            <h3>Vamos estudar sobre esse Framework</h3>
+        </div>
+    )
+}
